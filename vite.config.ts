665f8f22abc90817a58ec682c5aaa7501da83baa
pachinkotenshoku-ai <pachinkotenshoku@gmail.com>
2026-02-13 08:12:01 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: './',
+  define: {
+    // これが「真っ白」を防ぐための魔法の1行です
+    'process.env': {},
+  },
   build: {
     outDir: 'dist',
   },
